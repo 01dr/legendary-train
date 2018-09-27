36b328e0739c64b275c24fe1580299da8274174d
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-09-28 01:14:55 +0500

    beautify dux

--- src/redux/ducks/counter.js
+++ src/redux/ducks/counter.js
@@ -1,26 +1,17 @@
-const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
-const COUNTER_DECREMENT = 'COUNTER_DECREMENT';
+import { createAction, handleActions } from 'redux-actions';
 
 
-export const incrementCounter = () => ({
-  type: COUNTER_INCREMENT,
-});
-
-export const decrementCounter = () => ({
-  type: COUNTER_DECREMENT,
-});
+export const incrementCounter = createAction('COUNTER_INCREMENT');
+export const decrementCounter = createAction('COUNTER_DECREMENT');
 
 
-export default (state = 0, action) => {
-  switch (action.type) {
-    case COUNTER_INCREMENT:
-      return state + 1;
-    case COUNTER_DECREMENT:
-      return state - 1;
-    default:
-      return state;
-  }
-};
+const initialState = 0;
+
+
+export default handleActions({
+  [incrementCounter]: (state) => state + 1,
+  [decrementCounter]: (state) => state - 1
+}, initialState);
 
 
 export const getCounter = state => state.counter;
